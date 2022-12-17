55cf1348d1835291cd97a61d0d444e44e3543bb9
Hai Nguyen <bongnghin5@gmail.com>
2022-12-17 16:55:03 +0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState, useEffect } from "react"
+
+export default function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  
+  const toggleDarkMode = () => {
+    setDarkMode(darkMode => !darkMode) 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='App'>
+        <Navbar 
+          darkMode={darkMode}
+          toggleDarkMode={toggleDarkMode}
+        />
+        <Main 
+          darkMode={darkMode}
+        />
+      </div>
     </div>
-  );
+
+  )
 }
 
-export default App;
+function Navbar({darkMode, toggleDarkMode}) {
+  const darkModeTheme = darkMode ? "dark" : ''
+  return (
+    <nav className={`${darkModeTheme}`}>
+      <h1>Hihi</h1>
+      <div className="theme">
+        <p>Light</p>
+        <div className="toggle-darkmode" onClick={() => toggleDarkMode()}>
+          <div className="toggle-circle"></div>
+        </div>
+        <p>Black</p>
+      </div>
+    </nav>
+  )
+}
+
+function Main({darkMode}) {
+  const darkModeTheme = darkMode ? "dark" : ""
+  return (
+    <div className={`Main ${darkModeTheme}`}>
+      <p className='text'>{darkMode ? "Dark" : "White"}</p>
+    </div>
+  )
+}
